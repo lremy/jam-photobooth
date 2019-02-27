3f2f7cddc3582062e2cf4cdafa68f586f9db8896
Laurent REMY <lremy@is4ri.com>
2019-02-27 10:00:00 +0100

    update install shell

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 
 sudo raspi-config nonint do_camera 0
 sudo apt update
-sudo apt install python3-gpiozero python3-picamera python3-pip python3-pil git -y
-sudo pip3 install twython --upgrade
+sudo apt install python3-gpiozero python3-picamera python3-pip python3-pil fbi git -y
 cd ~
 [ -d Pictures ] || mkdir Pictures
-git clone https://github.com/raspberrypifoundation/jam-photobooth
+git clone https://github.com/lremy/jam-photobooth
 crontab -l | { cat; echo "@reboot python3 /home/pi/jam-photobooth/photobooth.py &"; } | crontab -
 sudo reboot
